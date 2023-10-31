ebb16095d71d00fe2b5d15acb1582c2c21368ecc
Lesculcay04 <116099796+Lesculcay04@users.noreply.github.com>
2023-10-30 21:45:52 -0500

    changes and delete

--- examen/src/main/kotlin/repository/ConferenceRepository.kt
+++
@@ -1,8 +0,0 @@
-package repository
-
-import model.Conference
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ConferenceRepository: JpaRepository<Conference, Long?> {
-    fun findById (id: Long?): Conference?
-}
